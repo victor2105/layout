03c236f746a224ac4233594d19b5f5727c4a5b35
Lucas Ferreira <lucasfercunha@gmail.com>
2014-04-11 20:16:22 -0300

    Lista itens no profile

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+$('#items-table .item').each(function() {
+  var name = $(this).find('img').data('name');
+  var description = $(this).find('img').data('description');
+  var value = $(this).find('img').data('value');
 
+  $(this).find('div').popover({
+    title: '<strong>' + name + '</strong>',
+    html: true,
+    trigger: 'hover',
+    placement: 'bottom',
+    content: description + '<br><strong>Value:</strong> <i class="fa fa-bitcoin"></i> ' + value
+  });
+});
