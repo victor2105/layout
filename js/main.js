2cc4b7b1bed538ce68a18641605512da1704289f
victor2105 <victor_henri_que@hotmail.com>
2014-04-28 20:57:33 -0300

    at #60

--- js/main.js
+++ js/main.js
@@ -9,19 +9,47 @@ var barChartData = {
     ]
 }
 
-var myLine = new Chart(document.getElementById("canvas_least").getContext("2d")).Bar(barChartData);
-var myLine = new Chart(document.getElementById("canvas_most").getContext("2d")).Bar(barChartData);
+if (document.getElementById("canvas_least"))
+  var myLine = new Chart(document.getElementById("canvas_least").getContext("2d")).Bar(barChartData);
+if (document.getElementById("canvas_most"))
+  var myLine = new Chart(document.getElementById("canvas_most").getContext("2d")).Bar(barChartData);
 
 $('#items-table .item').each(function() {
   var name = $(this).find('img').data('name');
   var description = $(this).find('img').data('description');
   var value = $(this).find('img').data('value');
+  var qtd_selling = ($(this).find('img').data('selling')) ? ' ' + $(this).find('img').data('selling') : '';
+  name += ($(this).hasClass('forsale')) ? ' <span class="badge">selling' + qtd_selling + '</span>' : '';
+  var qtd_trading = ($(this).find('img').data('trading')) ? ' ' + $(this).find('img').data('trading') : '';
+  name += ($(this).hasClass('fortrade')) ? ' <span class="badge">trading' + qtd_trading + '</span>' : '';
 
-  $(this).find('div').popover({
+  $(this).find('.item-tip').popover({
     title: '<strong>' + name + '</strong>',
     html: true,
     trigger: 'hover',
     placement: 'bottom',
     content: description + '<br><strong>Value:</strong> <span class="color-yellow"><i class="fa fa-bitcoin"></i> ' + value + '</span>'
   });
+
+  $(this).find('.item-tip').click(function() {
+    $('#modal-details').modal('show');
+  });
+});
+
+$('#selling-items-table tbody tr input').click(function() {
+  $('#modal-details').modal('show');
+});
+
+
+$(document).ready(function() {
+
+  $(".store-sale").owlCarousel({
+
+      navigation : false, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true
+
+  });
+
 });
