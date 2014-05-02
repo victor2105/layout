32e78add460ef76e5b865511ae72504a00690b70
Emerson <carvmerson@gmail.com>
2014-05-02 18:23:35 -0300

    Criado um arquivo javascript para o chat

---
+++ application/scripts/chat.js
@@ -0,0 +1,23 @@
+
+  var x = "#box";
+  $(x).hide();
+
+  $("#x").click(function(){
+    $("#box").show();
+  });
+
+  function message()
+  {
+    document.getElementById("chatScreen").innerHTML += "<br>" +  document.getElementById("textarea").value;
+    document.getElementById("textarea").value = "";
+    document.getElementById("chatScreen").scrollTop = document.getElementById("chatScreen").scrollHeight;
+  }
+
+  function buttonEnterPressed(){
+    if (event.keyCode == 13) {
+      this.message();
+      return false;
+    }else if(event.keyCode == 27){
+      $(x).hide();
+    }
+  }
