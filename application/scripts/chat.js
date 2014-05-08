3f1b7ac4b52a18dfba1f0fd7e853138f72b095e0
Emerson <carvmerson@gmail.com>
2014-05-08 10:48:36 -0300

    Lista de amigos agora é criada via javascript

--- application/scripts/chat.js
+++ application/scripts/chat.js
@@ -1,19 +1,32 @@
 var loged = false; // controle de log do usuário
 
 $("#box").hide(); // esconde o chat
-$(".chat-friend").hide();
+$(".chat-friend-list").hide();
+
+//$(".chat-box").css("right","700px");
+
+signIn(); // Loga
+//signOut(); // Desloga
+openListFriend();
+randomFriends();
 
 $(".friend-chat").click(function(){
     $("#box").show();
 });
 
-signIn(); // Loga
-//signOut(); // Desloga
-openListFriend();
+function randomFriends(){    
+    for(var i = 0; i < 5; i++){
+        var id = i;
+        
+        var txt = '<a href="#" class="list-group-item friend-chat" data-userid="'+id+'">Mario da burrinha <span class="fa icon fa-user pull-right color-green"></span></a>';    
+         $('.friend-list-group').append(txt);
+    }
+    
+}
 
 function openListFriend(){
     if(loged === true){
-        $(".chat-friend").show();
+        $(".chat-friend-list").show();
     }
 }
 
