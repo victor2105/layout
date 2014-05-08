a0978429ce53d4e8a1203d6262783dc2fecb3277
Emerson <carvmerson@gmail.com>
2014-05-08 09:07:45 -0300

    Alteração no script do chat, add função de signin e signout que futuramente devem se cominicar com as APIs

--- application/scripts/chat.js
+++ application/scripts/chat.js
@@ -1,23 +1,45 @@
+var loged = false; // controle de log do usuário
 
-  var x = "#box";
-  $(x).hide();
+$("#box").hide(); // esconde o chat
+$(".chat-friend").hide();
 
-  $(".friend-chat").click(function(){
+$(".friend-chat").click(function(){
     $("#box").show();
-  });
+});
 
-  function message()
-  {
+signIn(); // Loga
+//signOut(); // Desloga
+openListFriend();
+
+function openListFriend(){
+    if(loged === true){
+        $(".chat-friend").show();
+    }
+}
+
+function message()
+{
     document.getElementById("chatScreen").innerHTML += "<br>" +  document.getElementById("textarea").value;
     document.getElementById("textarea").value = "";
     document.getElementById("chatScreen").scrollTop = document.getElementById("chatScreen").scrollHeight;
-  }
+}
 
-  function buttonEnterPressed(){
+function signIn(){
+    //Deve-se usar as APIs para executar esta função
+    loged = true;
+}
+
+function signOut(){
+    //Deve-se usar as APIs para executar esta função
+    loged = false;
+}
+
+
+function buttonEnterPressed(){
     if (event.keyCode == 13) {
-      this.message();
-      return false;
+        this.message();
+        return false;
     }else if(event.keyCode == 27){
-      $(x).hide();
+        $("#box").hide();
     }
-  }
+}
