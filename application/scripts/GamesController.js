068118dede25b9894b3ad1d3e82e5e8d53c4e5da
luizrogeriocn <luizrogeriocn@gmail.com>
2014-05-25 05:27:38 -0300

    fix game controller and view

--- application/scripts/GamesController.js
+++ application/scripts/GamesController.js
@@ -1,10 +1,14 @@
 angular.module("istim").controller("GamesController", ['$scope', '$http', function(scope, http){
 	// -- mockup para servir de exemplo na view enquanto nao conecta com a api
 	scope.game1 = {title: 'Joguim dos leleks 2 - Leks Revenge', subtitle: 'Prepare-se para essa lelekagem e zueira.'};
-  	scope.games = [scope.game1];
+	scope.game2 = {title: 'dfhhgdhghghdhghdh', subtitle: 'Prepare-se para essa lelekagem e zueira.'};
+	scope.game3 = {title: '9334567875', subtitle: 'Prepare-se para essa lelekagem e zueira.'};
+	scope.game4 = {title: 'JodfdfsdsRevenge', subtitle: 'Prepare-se para essa lelekagem e zueira.'};
+	scope.game5 = {title: 'Josdsdsdos lelsdss Revenge', subtitle: 'Prepare-se para essa lelekagem e zueira.'};
+  	scope.games = [scope.game1, scope.game2, scope.game3, scope.game4, scope.game5];
   	// ----------------------------------------------------------------------
 
-	scope.game_list = [];
+	/*scope.game_list = [];
 	scope.api_url = "";
 
 	http.get(scope.api_url)
@@ -14,6 +18,6 @@ angular.module("istim").controller("GamesController", ['$scope', '$http', functi
 		})
 		.error(function(data) {
 			console.log('Error: ' + data);
-		});
+		});*/
 
 }]);
--- application/scripts/IstimController.js
+++ application/scripts/IstimController.js
@@ -3,8 +3,8 @@ var istim = angular.module( "istim", ['ngRoute'] );
 istim.config( function ( $routeProvider ) {
   $routeProvider
   .when( '/', { 
-        templateUrl: 'dashboard.html',
-        controller: 'IstimController'
+        //templateUrl: 'index.html',
+        //controller: 'IstimController'
     })
   .when( '/games', { 
         templateUrl: 'games.html',
@@ -13,6 +13,34 @@ istim.config( function ( $routeProvider ) {
   .when( '/game_show/:id', { 
         templateUrl: 'game_show.html',
         controller: 'GamesController'
+    })
+  .when( '/about', { 
+        templateUrl: 'about.html'//,
+        //controller: 'GamesController'
+    })
+  .when( '/contact_us', { 
+        templateUrl: 'contact_us.html'//,
+        //controller: 'GamesController'
+    })
+  .when( '/dashboard', { 
+        templateUrl: 'dashboard.html'//,
+        //controller: 'IstimController'
+    })
+  .when( '/developers', { 
+        templateUrl: 'developers.html'//,
+        //controller: 'GamesController'
+    })
+  .when( '/news', { 
+        templateUrl: 'news.html'//,
+        //controller: 'GamesController'
+    })
+  .when( '/ranking', { 
+        templateUrl: 'ranking.html'//,
+        //controller: 'GamesController'
+    })
+  .when( '/statistics', { 
+        templateUrl: 'statistics.html'//,
+        //controller: 'GamesController'
     })
   .otherwise( { redirectTo: '/' } );
 });
