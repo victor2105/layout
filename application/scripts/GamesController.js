5b14035931c5fce26b1caaff66c3db0bf63073f9
luizrogeriocn <luizrogeriocn@gmail.com>
2014-05-25 05:02:46 -0300

    implements http on games controller

--- application/scripts/GamesController.js
+++ application/scripts/GamesController.js
@@ -1,4 +1,19 @@
-angular.module("istim").controller("GamesController", ['$scope', function(scope){
-  scope.game1 = {title: 'Joguim dos leleks 2 - Leks Revenge', subtitle: 'Prepare-se para essa lelekagem e zueira.'};
-  scope.games = [scope.game1];
+angular.module("istim").controller("GamesController", ['$scope', '$http', function(scope, http){
+	// -- mockup para servir de exemplo na view enquanto nao conecta com a api
+	scope.game1 = {title: 'Joguim dos leleks 2 - Leks Revenge', subtitle: 'Prepare-se para essa lelekagem e zueira.'};
+  	scope.games = [scope.game1];
+  	// ----------------------------------------------------------------------
+
+	scope.game_list = [];
+	scope.api_url = "";
+
+	http.get(scope.api_url)
+		.success(function(data) {
+			scope.game_list = data;
+			console.log(data);
+		})
+		.error(function(data) {
+			console.log('Error: ' + data);
+		});
+
 }]);
